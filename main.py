38e34765af2a688e36d8184782eb22c03d139a60
Nicolas Lopes <nicolasmirandalopes@gmail.com>
2025-04-23 10:25:57 -0300

    adicionando sqlalchemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from utls import estoque
 
-estoque.Fornecedor.adicionar_fornecedor('Peladin', '40028922')
 estoque.Produto.listar_estoque()
 estoque.Produto.buscar_por_codigo('TECL01')
 estoque.Produto.listar_estoque()
