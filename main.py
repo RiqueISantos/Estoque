47a40dc97bc1638ba52abf44e5b2cef252bd664c
Nicolas Lopes <nicolasmirandalopes@gmail.com>
2025-05-09 14:43:00 -0300

    adicionando algumas funções

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from utls import estoque
 #exemplo de uso !
 #-------------------------------------------------------------------------------------------------------------------
-estoque.Categoria.adicionar_categoria('TEC')
-
+'''estoque.Categoria.adicionar_categoria('TEC')
+estoque.Marca.adicionar_marca('dell', 1)
 estoque.Fornecedor.adicionar_fornecedor('Gilson', '116532456')
 
 estoque.Produto.adicionar_produto('teclado', '123', 25, 'h098',1, 1)
 
 estoque.Produto.listar_estoque()
 
-estoque.Produto.buscar_por_codigo('123')
+estoque.Produto.buscar_por_codigo('123')'''
+
+#estoque.Produto.adicionar('mouse','158','30','h099',1,1)
+estoque.Marca.produto_marca('sangsung')
+
+estoque.Fornecedor.produto_fornecedor(1)
--- utls/estoque.py
+++ utls/estoque.py
@@ -1,6 +1,20 @@
 import datetime
+
+from bokeh.layouts import column
 from sqlalchemy import create_engine, Column, String, Integer, Date, ForeignKey
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+
+from abc import ABC, abstractmethod
+
+'''class Item(ABC):
+    @staticmethod
+    @abstractmethod
+    def adicionar(*args,**kwargs):
+        pass
+    @staticmethod
+    @abstractmethod
+    def remover(codigo):
+        pass'''
 
 # Configuração do banco
 db = create_engine('sqlite:///estoque.db')
@@ -15,12 +29,24 @@ class Fornecedor(Base):
     nome = Column(String,unique = True)
     contato = Column(String)
 
+    produtos = relationship('Produto', backref = 'fornecedores')
+
     @staticmethod
-    def adicionar_fornecedor(nome,contato):
+    def adicionar(nome,contato):
         fornecedor = Fornecedor(nome = nome,contato = contato)
         session.add(fornecedor)
         session.commit()
         print(f'o fornecedor {nome} foi adicionado')
+
+    @staticmethod
+    def produto_fornecedor(id_fornecedor):
+        fornecedor = session.query(Fornecedor).filter_by(id = id_fornecedor).first()
+        if not fornecedor:
+            print(f'fornecedor com id {id_fornecedor} não cadastrado no sistema')
+            return
+        print(f'Produtos do {fornecedor.nome}:')
+        for produto in fornecedor.produtos:
+            print(f'{produto.nome} - lote: {produto.lote}')
     
 class Categoria(Base):
     __tablename__ = 'categorias'
@@ -29,7 +55,7 @@ class Categoria(Base):
     nome = Column(String)
 
     @staticmethod
-    def adicionar_categoria(nome):
+    def adicionar(nome):
         categoria = Categoria(nome = nome)
         session.add(categoria)
         session.commit()
@@ -46,16 +72,26 @@ class Produto(Base):
     lote = Column(String)
     data_entrada = Column(Date, default=datetime.date.today)
     fornecedor_id = Column(Integer, ForeignKey('fornecedores.id'))
-    categoria_id = Column(Integer, ForeignKey('categorias.id'))
+    marca_id = Column(Integer, ForeignKey('marca.id'))
 
     @staticmethod
-    def adicionar_produto(nome, codigo, qtd, lote, fornecedor_id,categoria_id,data_entrada=None):
+    def adicionar(nome, codigo, qtd, lote, fornecedor_id,marca_id,data_entrada=None):
         if data_entrada is None:
             data_entrada = datetime.date.today()
-        produto = Produto(nome=nome, codigo=codigo, qtd=qtd, lote=lote, data_entrada=data_entrada, fornecedor_id = fornecedor_id,categoria_id = categoria_id)
+        produto = Produto(nome=nome, codigo=codigo, qtd=qtd, lote=lote, data_entrada=data_entrada, fornecedor_id = fornecedor_id,marca_id = marca_id)
         session.add(produto)
         session.commit()
         print(f"Produto {nome} adicionado com sucesso!")
+
+    @staticmethod
+    def remover(codigo):
+        produto = session.query(Produto).filter_by(codigo=codigo).first()
+        if not produto:
+            print(f'O produto {codigo} não foi cadastrado no sistema')
+            return
+        session.delete(produto)
+        session.commit()
+        print(f'O produto {produto.nome} com codigo {codigo} foi removido corretamente')
 
     @staticmethod
     def listar_estoque():
@@ -83,6 +119,31 @@ class Produto(Base):
             print(f"Data de entrada: {produto.data_entrada}")
         else:
             print(f"Nenhum produto com o código {codigo} foi encontrado.")
+
+class Marca(Base):
+    __tablename__ = 'marca'
+    id = Column(Integer, autoincrement = True, primary_key = True)
+    nome = Column(String)
+    id_categoria = Column(Integer,ForeignKey('categorias.id'))
+
+    produtos = relationship('Produto', backref = 'marca')
+
+    @staticmethod
+    def adicionar(nome,id_categoria):
+        marca = Marca(nome = nome, id_categoria = id_categoria)
+        session.add(marca)
+        session.commit()
+        print(f'a marca {nome} foi adicionada')
+
+
+    @staticmethod
+    def produto_marca(nome_marca):
+        marca = session.query(Marca).filter_by(nome=nome_marca).first()
+        if not marca:
+            print(f'a marca {nome_marca} não está registrada no sistema')
+            return
+        for produto in marca.produtos:
+            print(f'{produto.nome} - {produto.codigo}')
 
 # Criar as tabelas no banco
 Base.metadata.create_all(bind=db)
